5d58b9f0b968698f749d81c943469fd3a745b413
Irwin Li <irwin2014@gmail.com>
2016-08-31 15:53:10 -0400

    Added documentation for the amazon_lookup.py

--- amazon_lookup.py
+++ amazon_lookup.py
@@ -1,79 +1,18 @@
 
 import bottlenose, json, time, re
-import xml.etree.cElementTree as ElementTree
 from urllib.error import HTTPError
 from pprint import pprint
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
 
-##############################################################################
-# Pulled directly from
-# http://stackoverflow.com/questions/2148119/how-to-convert-an-xml-string-to-a-dictionary-in-python
-##############################################################################
-class XmlListConfig(list):
-    def __init__(self, aList):
-        for element in aList:
-            if element:
-                # treat like dict
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    self.append(XmlDictConfig(element))
-                # treat like list
-                elif element[0].tag == element[1].tag:
-                    self.append(XmlListConfig(element))
-            elif element.text:
-                text = element.text.strip()
-                if text:
-                    self.append(text)
-
-
-class XmlDictConfig(dict):
-    '''
-    Example usage:
-
-    >>> tree = ElementTree.parse('your_file.xml')
-    >>> root = tree.getroot()
-    >>> xmldict = XmlDictConfig(root)
-
-    Or, if you want to use an XML string:
-
-    >>> root = ElementTree.XML(xml_string)
-    >>> xmldict = XmlDictConfig(root)
-
-    And then use xmldict for what it is... a dict.
-    '''
-    def __init__(self, parent_element):
-        if parent_element.items():
-            self.update(dict(parent_element.items()))
-        for element in parent_element:
-            if element:
-                # treat like dict - we assume that if the first two tags
-                # in a series are different, then they are all different.
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    aDict = XmlDictConfig(element)
-                # treat like list - we assume that if the first two tags
-                # in a series are the same, then the rest are the same.
-                else:
-                    # here, we put the list in dictionary; the key is the
-                    # tag name the list elements all share in common, and
-                    # the value is the list itself 
-                    aDict = {element[0].tag: XmlListConfig(element)}
-                # if the tag has attributes, add those to the dict
-                if element.items():
-                    aDict.update(dict(element.items()))
-                self.update({element.tag: aDict})
-            # this assumes that if you've got an attribute in a tag,
-            # you won't be having any text. This may or may not be a 
-            # good idea -- time will tell. It works for the way we are
-            # currently doing XML configuration files...
-            elif element.items():
-                self.update({element.tag: dict(element.items())})
-            # finally, if there are no child tags and no attributes, extract
-            # the text
-            else:
-                self.update({element.tag: element.text})
 
 def error_handler(err):
+    """
+    @param err: error
+
+    This function handles errors recieved from the Amazon Product API
+    """
     ex = err['exception']
     if isinstance(ex, HTTPError) and ex.code == 503:
         time.sleep(random.expovariate(0.1))
@@ -91,6 +30,13 @@ amazon = bottlenose.Amazon(AMAZON_ACCESS_KEY,
 						   ErrorHandler=error_handler)
 
 def main():
+    """
+    Reads in asins from the asins.txt (one asin per line) and sends queries
+    the amazon product API. From the response, the asin, title, and product
+    group are recorded and written to a data file called 'test.txt'. The time
+    that the item was pulled is also recorded. The timestamp is useful since
+    items might become depricated and deleted from the database later.
+    """
 	try:
 		os.remove('test.txt')
 	except OSError:
